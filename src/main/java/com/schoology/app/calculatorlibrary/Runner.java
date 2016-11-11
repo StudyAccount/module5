b3afecc8f364d6b5ede2a2eb0d46cdacdf54bb16
SleepWalker <vityagorbenko@yandex.ru>
2016-11-12 01:14:56 +0200

    simple calculator realized and tested

--- src/main/java/com/schoology/app/calculatorlibrary/Runner.java
+++ src/main/java/com/schoology/app/calculatorlibrary/Runner.java
@@ -3,18 +3,31 @@ package com.schoology.app.calculatorlibrary;
 import com.schoology.app.calculatorlibrary.calculator.Calculator;
 import com.schoology.app.calculatorlibrary.calculator.SimpleCalculator;
 
+import java.util.Scanner;
+
 /**
  * Created by User on 06.10.2016.
  */
 public class Runner {
     public static void main(String[] args) {
+
         Calculator simpleCalculator = new SimpleCalculator();
+        Scanner scanner = new Scanner(System.in);
 
         simpleCalculator.init();
-        String currentOperation = simpleCalculator.calculate("*,22.2,100d");
 
+        while (true) {
 
-        System.out.println(currentOperation);
+            System.out.println("Enter expression");
+            String input = scanner.nextLine();
+
+            if ("quit".equals(input)){
+                break;
+            }
+
+            String currentOperation = simpleCalculator.calculate(input);
+            System.out.println("Result " + currentOperation);
+        }
 
     }
 
--- src/test/java/com/schoology/app/calculatorlibrary/calculator/SimpleCalculatorTest.java
+++ src/test/java/com/schoology/app/calculatorlibrary/calculator/SimpleCalculatorTest.java
@@ -1,21 +1,12 @@
 package com.schoology.app.calculatorlibrary.calculator;
 
-import com.schoology.app.calculatorlibrary.operations.*;
-import com.schoology.app.calculatorlibrary.provider.SimpleOperationProvider;
-import com.schoology.app.calculatorlibrary.types.DoubleSupportedType;
-import com.schoology.app.calculatorlibrary.types.LongSupportedType;
-import com.schoology.app.calculatorlibrary.types.SupportedType;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by User on 05.10.2016.
@@ -24,8 +15,6 @@ import java.util.List;
 @RunWith(value = Parameterized.class)
 public class SimpleCalculatorTest {
 
-    private  List<SupportedType> supportedTypesList = new ArrayList<>();
-    private  List<Operation> supportedOperations = new ArrayList<>();
     private static final SimpleCalculator simpleCalculator = new SimpleCalculator();
     private static String  inputData;
     private static String expectation;
@@ -35,19 +24,6 @@ public class SimpleCalculatorTest {
         this.inputData = inputData;
         this.expectation = expectation;
     }
-//
-//    @BeforeClass
-//    public static void setUp() throws Exception {
-//
-//        supportedTypesList.add(new LongSupportedType());
-//        supportedTypesList.add(new DoubleSupportedType());
-//
-//        supportedOperations.add(new AdditionLong());
-//        supportedOperations.add(new AdditionDouble());
-//        supportedOperations.add(new SubtractionLong());
-//        supportedOperations.add(new SubtractionDouble());
-//
-//    }
 
     @Parameterized.Parameters
     public static Collection<Object[]> getData() {
@@ -56,29 +32,19 @@ public class SimpleCalculatorTest {
         String c = "+,2D,2.2";
         String d = "+,qwe,asd";
         return Arrays.asList(new Object[][]{
-                {a, ""},
-                {b, ""},
-                {c, ""},
-                {d, ""},
+                {a, "Current operation is not supported"},
+                {b, "0.0"},
+                {c, "4.2"},
+                {d, "Current types are not supported"},
         });
-
     }
 
     @Test
     public void testCalculate() throws Exception {
 
-        supportedTypesList.add(new LongSupportedType());
-        supportedTypesList.add(new DoubleSupportedType());
-
-        supportedOperations.add(new AdditionLong());
-        supportedOperations.add(new AdditionDouble());
-        supportedOperations.add(new SubtractionLong());
-        supportedOperations.add(new SubtractionDouble());
-
-       final String result = simpleCalculator.calculate(inputData);
-        Assert.assertEquals("s", new String[]{expectation},  new String[]{result});
-
-
+        simpleCalculator.init();
+        final String result = simpleCalculator.calculate(inputData);
+        Assert.assertArrayEquals(new String[]{expectation},  new String[]{result});
     }
 
 }
